a52137204f6e82cd1db2019f7236c75cd1180280
Skyla <skylaclark094@gmail.com>
2023-01-29 10:42:53 -0500

    Added admin and Coach

---
+++ src/main/java/me/skyla/pathpeak/server/objects/Admin.java
@@ -0,0 +1,9 @@
+package me.skyla.pathpeak.server.objects;
+
+/**
+ * Admin class, super high level shit
+ * Author: Skye
+ * 1/29/2023
+ */
+public class Admin extends Coach{
+}
---
+++ src/main/java/me/skyla/pathpeak/server/objects/Coach.java
@@ -0,0 +1,9 @@
+package me.skyla.pathpeak.server.objects;
+
+/**
+ * Coach class, extends user to inherit all permissions
+ * Author: Skye
+ * 1/29/2023
+ */
+public class Coach extends User{
+}
